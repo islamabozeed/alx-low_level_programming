c0be62a0a835635d6b485244cfe7060deb5f14dc
islamabozeed <islammahmoud13@gmail.com>
2023-03-21 11:30:41 -0700

    I made it yaa

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,12 @@
  */
 int print_last_digit(int n)
 {
-int a;
-if (n < 0)
-	n = -n;
+	int last_digit = n % 10;
 
-	a = n % 10;
-	if (a < 0)
-		a = -a;
-	_putchar(a + '0');	
-	return (a);
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
