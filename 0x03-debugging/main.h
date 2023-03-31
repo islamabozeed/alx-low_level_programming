e17c5e06136d4d10eba9abb9002acdd44011c64f
islamabozeed <islammahmoud13@gmail.com>
2023-03-31 04:15:11 -0700

    debugging correct2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,14 +2,11 @@
 #define MAIN_H
 #include <stdio.h>
 /*
- * this is the main for initiat all functions
- *
- */
-
+* this is the main for initiat all functions
+*
+*/
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-
 #endif
-
