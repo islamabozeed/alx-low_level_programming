b8f14b735774e80afdb8d181ef163b24b03d0610
islamabozeed <islammahmoud13@gmail.com>
2023-04-05 17:36:08 -0700

    always first3

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,11 +2,23 @@
 /**
  * _sqrt_recursion - square root
  * @n: the number
+ * sqrt - helper func
+ * @v: helper value
  * Return: result
  */
+int sqrt(int n, int v);
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (_sqrt_recursion(n, 0));
+	return (sqrt(n, 1));
 }
+/*
+ * sqrt - helper func
+ */
+int sqrt(int n, int v)
+{
+	if (v * v == n)
+		return (v);
+	else if (v * v < n)
+		return (sqrt(n, v + 1));
+	else
+		return (-1);
