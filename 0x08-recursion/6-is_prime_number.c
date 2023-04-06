448e6181498fe633578b1b16adfc5e84a3d5aa78
islamabozeed <islammahmoud13@gmail.com>
2023-04-05 18:01:56 -0700

    first is first1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,9 +4,23 @@
  * @n: number
  * Return: result
  */
+int _helper(int n, int v);
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	return (_helper(n, 2));
+}
+/**
+ * _helper - for checking prime n
+ * @n: number
+ * @v number
+ * Return: zeroo
+ */
+int _helper(int n, int v)
+{
+	if (v >= n && n > 1)
+		return (1);
+	else if (n % v == 0 || n <= 1)
 		return (0);
-	return (is_prime_number(n, n - 1));
+	else
+		return (_helper(n, v + 1));
 }
