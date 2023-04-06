d5d03cfe3d39166c6c57e7ca8c9d6c4f88b199fe
islamabozeed <islammahmoud13@gmail.com>
2023-04-05 17:56:12 -0700

    first is first

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * _sqrt_recursion - square root
+ * _sqrt - helper func
+ * @v: helper value
  * @n: the number
  * Return: result
  */
@@ -10,9 +12,11 @@ int _sqrt_recursion(int n)
 	return (_sqrt(n, 1));
 }
 /**
- * _sqrt - helper func
- * @v: helper value
- */
+ *_sqrt - helper func
+ *@v: helper value
+ *@n: helper value
+ *Return: alwayz zero
+*/
 int _sqrt(int n, int v)
 {
 	if (v * v == n)
