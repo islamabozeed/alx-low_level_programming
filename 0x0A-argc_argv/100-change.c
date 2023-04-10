85a3eedb7dac393913368cdf7b6b27b515da4cf4
islamabozeed <islammahmoud13@gmail.com>
2023-04-10 12:58:48 -0700

    argc & argv[]

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - program that prints its name, followed by a new line
  * @argc: number
  * @argv: array
  * Return: zero
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	if (argc == 2)
+	{
+		int i, leastcents = 0, money = atoi(atgv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				leastcents += money / cents[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", leastcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
