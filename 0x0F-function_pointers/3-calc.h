d7b1758ce4bdfb0bac6e292f98c64ed3175e63f1
islamabozeed <islammahmoud13@gmail.com>
2023-04-19 10:36:54 -0700

    function9

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,10 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
- * struct op - operator strcut
- * @op: oprtsyot
- * @f: function assoc
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
